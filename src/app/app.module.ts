980dff888d49e2b6bc58306f06bebff8b31a32e5
martinfruehauf <martin.fruehauf@openknowledge.de>
2020-06-03 16:20:12 +0200

    Delete detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TodosComponent } from './todos/todos.component';
 import { FormsModule } from '@angular/forms';
-import { TodoDetailComponent } from './todo-detail/todo-detail.component'; // <-- NgModel lives here
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
-    TodoDetailComponent
   ],
   imports: [
     BrowserModule,
