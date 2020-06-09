76d72cb9d17953795b5e8f887cab5dc0dad9a189
martinfruehauf <martin.fruehauf@openknowledge.de>
2020-06-09 09:38:09 +0200

    Change cards

--- src/app/mock-todos.ts
+++ src/app/mock-todos.ts
@@ -6,20 +6,20 @@ export const TODOS: Todo[] = [
     name: 'mock-todo 1',
     description: 'this is a description of the todo 1',
     status: true,
-    dueDate: new Date(2017, 4, 4, 17, 23, 42, 11)
+    dueDate: new Date('2020-01-10T07:30')
   },
   {
     id: 2,
     name: 'mock-todo 2',
     description: 'this is a description of the todo 2',
     status: false,
-    dueDate: new Date(2018, 4, 4, 17, 23, 42, 11)
+    dueDate: new Date('2019-01-10T07:30')
   },
   {
     id: 3,
     name: 'mock-todo 3',
     description: 'this is a description of the todo 2',
     status: true,
-    dueDate: new Date(2019, 4, 4, 17, 23, 42, 11)
+    dueDate: new Date('2018-01-10T07:30')
   }
 ];
