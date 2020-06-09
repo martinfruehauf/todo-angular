1061d1740b26f7d84a2ea2dc3d7ac9e897c0dfa1
martinfruehauf <martin.fruehauf@openknowledge.de>
2020-06-09 13:36:53 +0200

    Remove unused code

--- src/app/todo-detail-modal/todo-detail-modal.component.ts
+++ src/app/todo-detail-modal/todo-detail-modal.component.ts
@@ -1,6 +1,5 @@
-import {Component, Input} from '@angular/core';
+import { Component } from '@angular/core';
 
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { Todo } from '../todo';
 
 @Component({
@@ -9,26 +8,7 @@ import { Todo } from '../todo';
   styleUrls: ['./todo-detail-modal.component.css']
 })
 export class TodoDetailModalComponent {
-  closeResult = '';
-  @Input() todo: Todo;
+  private todo: Todo;
 
-  constructor(private modalService: NgbModal) {}
-
-  open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
-  }
+  constructor() {}
 }
