a451f3ab8e1bd123434639733b3e2c794c24c25b
martinfruehauf <martin.fruehauf@openknowledge.de>
2020-06-16 11:17:07 +0200

    Change test

--- src/app/todo-detail-modal/todo-detail-modal.component.spec.ts
+++ src/app/todo-detail-modal/todo-detail-modal.component.spec.ts
@@ -4,6 +4,31 @@ import { TodoDetailModalComponent } from './todo-detail-modal.component';
 import { Todo } from '../todo';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {TodosComponent} from '../todos/todos.component';
+import {TodoService} from '../todo.service';
+import {Observable, of} from 'rxjs';
+import {TODOS} from '../mock-todos';
+
+class MockTodoService{
+  public getTodos(): Observable<Todo[]> {
+    return of(TODOS);
+  }
+}
+
+class MockNgbModal {
+  public modalRef: {
+    componentInstance: {
+      todo: Todo
+    }
+  };
+  public open(todo: Todo) {
+    this.modalRef = {
+      componentInstance: {
+        todo
+      }
+    };
+    return this.modalRef;
+  }
+}
 
 describe('TodoDetailModalComponent', () => {
   let component: TodoDetailModalComponent;
@@ -11,7 +36,17 @@ describe('TodoDetailModalComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodoDetailModalComponent ]
+      declarations: [ TodoDetailModalComponent],
+      providers: [
+        {
+          provide: TodoService,
+          useClass: MockTodoService
+        },
+        {
+          provide: NgbModal,
+          useClass: MockNgbModal
+        }
+      ]
     })
     .compileComponents();
   }));
@@ -19,12 +54,13 @@ describe('TodoDetailModalComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TodoDetailModalComponent);
     component = fixture.componentInstance;
+    console.log('Created component Instance', component);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     console.log('!!!!!! COMPONENT:', component);
-    console.log('!!!');
+    console.log('!!!!!!');
     expect(component).toBeTruthy();
   });
 });
