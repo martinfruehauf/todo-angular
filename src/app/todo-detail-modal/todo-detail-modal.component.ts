5a08b5114cb24e0f0491034ad7b7e5eafc8c8903
martinfruehauf <martin.fruehauf@openknowledge.de>
2020-06-16 17:37:07 +0200

    Add inputs for the modal

--- src/app/todo-detail-modal/todo-detail-modal.component.ts
+++ src/app/todo-detail-modal/todo-detail-modal.component.ts
@@ -1,15 +1,44 @@
-import {Component, Input} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 
 import { Todo } from '../todo';
+import {BaseTodoDTO} from '../baseTodoDTO';
 
 @Component({
   selector: 'app-todo-detail-modal',
   templateUrl: './todo-detail-modal.component.html',
   styleUrls: ['./todo-detail-modal.component.css']
 })
-export class TodoDetailModalComponent {
-  @Input()
-  public todo: Todo;
+export class TodoDetailModalComponent implements AfterViewInit, OnInit{
+  @Input() public todo: Todo;
+  @Input() public baseTodoDTO: BaseTodoDTO;
+  @ViewChild('name') name: ElementRef;
+  @ViewChild('description') description: ElementRef;
+  @ViewChild('status') status: ElementRef;
+  @ViewChild('dueDate') dueDate: ElementRef;
 
-  constructor() {}
+  constructor() {
+    this.baseTodoDTO = {
+      name: '',
+      description: '',
+      status: false,
+      dueDate: null
+    };
+  }
+
+  public save(){
+    console.log('//&&&///&&& TODO', this.todo);
+    console.log('//&&&///&&& BASETODO', this.baseTodoDTO);
+    this.baseTodoDTO.name = this.name.nativeElement.value;
+    this.baseTodoDTO.description = this.description.nativeElement.value;
+    this.baseTodoDTO.status = this.status.nativeElement.checked;
+    this.baseTodoDTO.dueDate = new Date(this.dueDate.nativeElement.value);
+    console.log('//&&&///&&&?????????? BASETODO NAME', this.baseTodoDTO);
+  }
+
+  ngAfterViewInit() {
+  }
+
+  ngOnInit(): void {
+
+  }
 }
